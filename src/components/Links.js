3db601f93def3276d1c7543f9398679b1af1386e
Bashu-011 <bashu2609@gmail.com>
2024-01-30 18:01:13 +0300

    Prctice lab

---
+++ src/components/Links.js
@@ -0,0 +1,10 @@
+export default function Links({linkedin, github}){
+    return(
+        <div>
+        <h3>Links</h3>
+        <a href={linkedin}>https://github.com/liza</a>
+        <a href={github}>https://www.linkedin.com/in/liza/</a>
+        </div>
+    )
+}
+
